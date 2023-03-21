2702fe7599f72286c30b7da01fc51aba328718ce
Adrian Gould <Adrian.Gould@nmtafe.wa.edu.au>
2023-03-21 14:38:19 +0800

    feat: Countries
    - Example country migration and seeder

--- database/seeders/CountrySeeder.php
+++ database/seeders/CountrySeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Country;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,13 @@ class CountrySeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $seedCountries = [
+
+        ];
+
+        foreach ($seedCountries as $country) {
+            Country::create($country);
+        }
+
     }
 }
