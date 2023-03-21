289f588cef936d78c01530cc630d236bf23cfc40
Adrian Gould <Adrian.Gould@nmtafe.wa.edu.au>
2023-03-21 14:38:51 +0800

    feat: Formats
    - Example Format seeder

--- database/seeders/FormatSeeder.php
+++ database/seeders/FormatSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Format;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,12 @@ class FormatSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $seedFormats = [
+
+        ];
+
+        foreach ($seedFormats as $format) {
+            Format::create($format);
+        }
     }
 }
